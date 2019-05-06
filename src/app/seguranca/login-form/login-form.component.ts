b9dcf7ae0da924689e9d4ebdb9266d9b7d2c0e6b
Pedro Bacchini <pedrobacchini@outlook.com>
2019-05-06 13:52:19 -0300

    Adicionando o redirecionamento para /lancamento quando for sucesso no login e limpando a senha quando uma tentativa de login falhar.

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { ErrorHandlerService } from '../../core/error-handler.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -13,7 +14,8 @@ export class LoginFormComponent implements OnInit {
 
   constructor(
     private auth: AuthService,
-    private errorHandler: ErrorHandlerService
+    private errorHandler: ErrorHandlerService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -21,6 +23,9 @@ export class LoginFormComponent implements OnInit {
 
   login(usuario: string, senha: string) {
     this.auth.login(usuario, senha)
+      .then(() => {
+        this.router.navigate(['/lancamentos']);
+      })
       .catch(error => {
         this.errorHandler.handle(error);
       });
