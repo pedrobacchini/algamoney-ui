76afc36dd0e92fd2f5679a6cbb6d4f269501afff
Pedro Bacchini <pedro.bacchini@cilia.com.br>
2019-10-23 09:16:31 -0300

    Settings server info.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
-  tokenWhitelistedDomains: [ /localhost:8080/ ],
+  apiUrl: 'http://localhost:7979',
+  tokenWhitelistedDomains: [ /localhost:7979/ ],
   tokenBlacklistedDomains: [ /\/oauth\/token/ ]
 };
 
